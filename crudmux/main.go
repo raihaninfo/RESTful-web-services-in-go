46c3d97fe93efd69fce0143b1be24d190f35be80
raihaninfo <raihanmahmudi35@gmail.com>
2022-03-16 20:46:53 +0600

    all cars by id

--- crudmux/main.go
+++ crudmux/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -17,7 +19,7 @@ type Vehicle struct {
 var vehicle = []Vehicle{
 	{1, "Toyota", "Corolla", 10000},
 	{2, "Toyota", "Camry", 20000},
-	{1, "Honda", "Civic", 15000},
+	{3, "Honda", "Civic", 15000},
 }
 
 func allCars(w http.ResponseWriter, r *http.Request) {
@@ -25,10 +27,39 @@ func allCars(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(vehicle)
 }
 
+func allCarsById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	carId, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, car := range vehicle {
+		if car.Id == carId {
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(car)
+		}
+	}
+}
+
+func updateCars(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func addCars(w http.ResponseWriter, r *http.Request) {
+
+}
+func deleteCars(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/cars", allCars).Methods("GET")
+	router.HandleFunc("/cars/{id}", allCarsById).Methods("GET")
+	router.HandleFunc("/cars/{id}", updateCars).Methods("PUT")
+	router.HandleFunc("/cars", addCars).Methods("POST")
+	router.HandleFunc("/cars/{id}", deleteCars).Methods("DELETE")
 	http.ListenAndServe(":8081", router)
 
 }
